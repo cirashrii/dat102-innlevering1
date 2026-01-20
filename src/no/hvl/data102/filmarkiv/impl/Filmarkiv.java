acbb9033127bae597064440f48340fcc1dd41bfc
cirashrii <cirashrii@gmail.com>
2026-01-20 14:17:15 +0100

    aaaaaaaaa

--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -26,6 +26,41 @@ public class Filmarkiv {
         }
     }
 
+    @Override
+    public Film finnFilm(int nr) {
+
+        return Film[nr];
+    }
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        return false;
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        return new Film[0];
+    }
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        return new Film[0];
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        return 0;
+    }
+
+    @Override
+    public int antall() {
+        return 0;
+    }
 
     private Film[] trimTab(Film[] tab, int n) {
 // n er antall elementer
