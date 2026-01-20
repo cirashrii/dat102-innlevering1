a121a455bddfa4eb84ae37d0e18975e10637a42c
cirashrii <cirashrii@gmail.com>
2026-01-20 13:23:22 +0100

    1.2 ferdig

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import no.hvl.data102.filmarkiv.impl.*;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Film aaa = new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios");
+        Film bbb = new Film(1,"Grethe", "Proj 2 electric boogaloo", 2007, Sjanger.ACTION, "StudioStudios");
+
+        // Hvis samme nummer skrives ut, fix override 1.1
+
+    }
+
+}
