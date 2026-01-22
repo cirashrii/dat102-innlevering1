f4735e5bbb9f3c88af305eb3fde0951bbd7574e0
cirashrii <cirashrii@gmail.com>
2026-01-22 11:35:10 +0100

    klarte å lage en test yayyyy

--- src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -1,11 +1,17 @@
 package no.hvl.data102.filmarkiv.test;
 
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 
 class FilmarkivTest {
 
     @Test
     void dobleTabell() {
+
     }
 
     @Test
@@ -34,5 +40,10 @@ class FilmarkivTest {
 
     @Test
     void testAntall() {
+        Filmarkiv helloworld = new Filmarkiv(4);
+        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+
+        assertEquals(helloworld.antall(),2);
     }
 }
