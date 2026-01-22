c435d0f368c0d499e85b05988365b15c91da5259
RagnhildSG <Ragnhild2002@hotmail.com>
2026-01-22 10:46:29 +0100

    Slett

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Film aaa = new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios");
         Film bbb = new Film(1,"Grethe", "Proj 2 electric boogaloo", 2007, Sjanger.ACTION, "StudioStudios");
 
+
         // Hvis samme nummer skrives ut, fix override 1.1
 
 
