5534bcf8fee566987e20bf46dd68c265bb7f7b8a
cirashrii <cirashrii@gmail.com>
2026-01-22 12:48:08 +0100

    klient skal være tilgjengelig no

---
+++ src/no/hvl/data102/filmarkiv/klient/FilmarkivMain.java
@@ -0,0 +1,11 @@
+package no.hvl.data102.filmarkiv.klient;
+
+public class FilmarkivMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! -Grethe");
+
+    }
+
+}
