ef185121b37add082c36869d22397b8ac1be346a
RagnhildSG <Ragnhild2002@hotmail.com>
2026-01-22 13:03:45 +0100

    temp

--- src/no/hvl/data102/filmarkiv/klient/Meny.java
+++ src/no/hvl/data102/filmarkiv/klient/Meny.java
@@ -1,4 +1,24 @@
 package no.hvl.data102.filmarkiv.klient;
 
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+
 public class Meny {
+        private Tekstgrensesnitt tekstgr;
+        private FilmarkivADT filmarkiv;
+        public Meny(FilmarkivADT filmarkiv){
+            tekstgr = new Tekstgrensesnitt();
+            this.filmarkiv = filmarkiv;
+        }
+        public void start(){
+// legg inn en del forhåndsdefinerte filmer for å teste metodene
+// ..
+// TODO
+            Film aaa = new Film(3,"George", "Wars", 1995, Sjanger.ACTION, "Disney");
+
+
+        }
+
+
 }
