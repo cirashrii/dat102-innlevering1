0275e68e90f3073943ce152e1099866f2c379753
cirashrii <cirashrii@gmail.com>
2026-01-22 12:20:52 +0100

    tutlet med en god del forskjellig men har generelt sett jobbet med finnfilm og dobletabell

--- src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -6,16 +6,34 @@ import no.hvl.data102.filmarkiv.impl.Sjanger;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 class FilmarkivTest {
 
     @Test
     void dobleTabell() {
-
+//        Filmarkiv helloworld = new Filmarkiv(2);
+//        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+//        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+//        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+//
+//        assertEquals(helloworld.antall(),3);
+        //out of bounds error, dobletabell fungerer ikke
+        //vet ikke hvordan jeg sjekker dobletabell direkte (uten å legge til metoder), må istedet satse på antall
     }
 
     @Test
     void finnFilm() {
+        Filmarkiv samling = new Filmarkiv(4);
+        Film aaa = new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios");
+        Film bbb = new Film(2,"Grethe", "Proj 2 electric boogaloo", 2007, Sjanger.DRAMA, "StudioStudios");
+
+
+        samling.leggTilFilm(aaa);
+        samling.leggTilFilm(bbb);
+
+        assertEquals(samling.finnFilm(1),aaa);
+        assertEquals(samling.finnFilm(2),bbb);
     }
 
     @Test
@@ -44,10 +62,10 @@ class FilmarkivTest {
 
     @Test
     void testAntall() {
-        Filmarkiv helloworld = new Filmarkiv(4);
-        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
-        helloworld.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+        Filmarkiv samling = new Filmarkiv(4);
+        samling.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.ACTION, "StudioStudios"));
+        samling.leggTilFilm(new Film(1,"Grethe", "Proj Filmen", 2005, Sjanger.DRAMA, "StudioStudios"));
 
-        assertEquals(helloworld.antall(),2);
+        assertEquals(samling.antall(),2);
     }
 }
