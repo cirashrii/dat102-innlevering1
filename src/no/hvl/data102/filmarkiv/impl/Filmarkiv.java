5db7bbc5eaa14a3662e989d3a2c49feaa44440f9
cirashrii <cirashrii@gmail.com>
2026-01-22 10:49:31 +0100

    antall fikset

--- src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -69,7 +69,7 @@ public class Filmarkiv implements FilmarkivADT {
 
     @Override
     public int antall() {
-        return 0;
+        return antall;
     }
 
     private Film[] trimTab(Film[] tab, int n) {
