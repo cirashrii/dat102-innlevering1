87be36a3f774c7df2c471b1ee9fd9881e5b89e3a
cirashrii <cirashrii@gmail.com>
2026-01-22 11:07:58 +0100

    test opprettet (ihvertfall basisen sånn at vi kan tutle)

--- src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -1,7 +1,38 @@
 package no.hvl.data102.filmarkiv.test;
 
-public class FilmarkivTest {
+import org.junit.jupiter.api.Test;
 
-    //vi tester her :)
+class FilmarkivTest {
 
+    @Test
+    void dobleTabell() {
+    }
+
+    @Test
+    void finnFilm() {
+    }
+
+    @Test
+    void leggTilFilm() {
+    }
+
+    @Test
+    void slettFilm() {
+    }
+
+    @Test
+    void soekTittel() {
+    }
+
+    @Test
+    void soekProdusent() {
+    }
+
+    @Test
+    void antall() {
+    }
+
+    @Test
+    void testAntall() {
+    }
 }
