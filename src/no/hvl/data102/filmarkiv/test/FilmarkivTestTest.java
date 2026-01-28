72eb96f7dba897c351c7294dd232328591fd7d36
cirashrii <cirashrii@gmail.com>
2026-01-28 13:41:24 +0100

    lærer hvordan teste ting som ordentlig utvikler

---
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTestTest.java
@@ -0,0 +1,41 @@
+package no.hvl.data102.filmarkiv.test;
+
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FilmarkivTestTest {
+
+    // Given
+    // Setter opp testen
+    // Forventning, hva skal testen produsere? Start with why
+
+    // When
+    // Kjører testen
+
+    // Then
+    // Evaluerer resultatet, sjekker om man får ønsket resultat
+
+    @Test
+    void leggTilFilm_skalLeggeTilFilmIArray() {
+        // Given
+        Film expected = new Film(0, "Ashley", "Loaf Cat: Loafening", 2028, Sjanger.DRAMA, "Ashley");
+        Filmarkiv test = new Filmarkiv(50);
+
+        // When
+        test.leggTilFilm(expected);
+
+        // Then
+        Assertions.assertEquals(1,test.antall(),1);
+    }
+
+}
